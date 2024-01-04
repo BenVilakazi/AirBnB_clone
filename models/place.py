4ba046f028c94359fda96d149f0901bc8ba4c40a
Ben Vilakazi <xolanithengubane@gmail.com>
2024-01-04 08:06:36 +0200

    we

---
+++ models/place.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class Place Inherits from BaseModel"""
+
+import models
+
+class Place(models.BaseModel):
+    """Class to Store place information"""
+    city_id = ""
+    user_id = ""
+    name = ""
+    descrption = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    lat
