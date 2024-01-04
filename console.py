3c9e8cdb899d4da596452b2c519253cd266ea397
Ben Vilakazi <xolanithengubane@gmail.com>
2024-01-04 20:17:24 +0200

    emptyline

--- console.py
+++ console.py
@@ -7,7 +7,33 @@ import models
 import re
 
 class HBNBCommand(cmd.Cmd):
-    pass
+    """Class Console"""
+    prompt = '(bnb)'
+    
+    def precmd(self, line):
+        return line.strip()
+    
+    def do_EQF(self, line):
+        """_summary_
+
+        EQF:
+            End of (_file_): _Exit console_
+        """
+        return True
+    
+    def do_quit(self, line):
+        """_summary_
+
+        Args:
+        quit : _exits the console_
+        """
+        
+        return True
+    
+    def emptyline(self):
+        pass
+    
+    
     
 if __name__ == '__main__':
     """command loop"""
