ad1f05a4b1a3208b2a9c0d1830a6879652abe657
Ben Vilakazi <xolanithengubane@gmail.com>
2024-01-04 07:58:41 +0200

    city

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class City that inherits from BaseModel"""
+
+import models
+
+class City(models.BaseModel):
+    """Class implements City"""
+    state_id = ""
+    name = ""
