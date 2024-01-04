3f6b46f42e088644c34584592679e68857285471
Ben Vilakazi <xolanithengubane@gmail.com>
2024-01-04 07:50:48 +0200

    state

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class State that inherits from BaseModel"""
+
+import models
+
+
+class State(models.BaseModel):
+    """Class State that inherits from BaseModel"""
+    name = ""
