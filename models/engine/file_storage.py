7ea2adf8ee455bf2736f906cd24f46e3ab1cf9d5
Ben Vilakazi <xolanithengubane@gmail.com>
2024-01-05 20:49:04 +0200

    whar

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,13 +22,26 @@ class FileStorage:
     __objects = {}
     
 def all(self):
-    pass
+    """returns dict"""
+    return self.__objects 
 
 def new(self):
-    pass
-
+    """sets the obj with key"""
+    key = obj.__class__.__name__+ '.' + obj.id
+    self.__objects[key] = obj
+    
 def save(self):
-    pass
+    """serialize objects to the JSON file"""
+    with open(self.__file_path, 'w') as f:
+        jdict = ()
+        for name, obj in self.__objects.items():
+            jdict[name] = obj.to_dict()
+        json.dump(jdict, f)
 
 def reload(self):
-    pass
+    """deserialize JSON file"""
+    try:
+        with open(self.__file_path, 'r') as f:
+            self.__objects = json.load(f, object_hook=models_obj_hook)
+    except:
+        pass
