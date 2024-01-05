e0e94bbd41121ccccacb3d798337ee6e0eca2668
Ben Vilakazi <xolanithengubane@gmail.com>
2024-01-05 20:53:47 +0200

    console test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Store first obj"""
+"""Store first object"""
 
 import models
 import json
 
+
 def models_obj_hook(o_dict):
-    """Import BaseModel from models and returms dict"""
+    """imports BaseModel from models and returns dict"""
     try:
         cls = o_dict['__class__']
     except KeyError:
@@ -15,33 +16,34 @@ def models_obj_hook(o_dict):
             return getattr(models, cls)(**o_dict)
         except AttributeError:
             return o_dict
-        
+
+
 class FileStorage:
-    """created private class attr"""
+    """created private class attributes"""
     __file_path = "file.json"
     __objects = {}
-    
-def all(self):
-    """returns dict"""
-    return self.__objects 
 
-def new(self):
-    """sets the obj with key"""
-    key = obj.__class__.__name__+ '.' + obj.id
-    self.__objects[key] = obj
-    
-def save(self):
-    """serialize objects to the JSON file"""
-    with open(self.__file_path, 'w') as f:
-        jdict = ()
-        for name, obj in self.__objects.items():
-            jdict[name] = obj.to_dict()
-        json.dump(jdict, f)
+    def all(self):
+        """returns dictionary"""
+        return self.__objects
 
-def reload(self):
-    """deserialize JSON file"""
-    try:
-        with open(self.__file_path, 'r') as f:
-            self.__objects = json.load(f, object_hook=models_obj_hook)
-    except:
-        pass
+    def new(self, obj):
+        """sets the obj with key"""
+        key = obj.__class__.__name__ + '.' + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        with open(self.__file_path, 'w') as f:
+            jdict = {}
+            for name, obj in self.__objects.items():
+                jdict[name] = obj.to_dict()
+            json.dump(jdict, f)
+
+    def reload(self):
+        """deserializes the JSON file"""
+        try:
+            with open(self.__file_path, 'r') as f:
+                self.__objects = json.load(f, object_hook=models_obj_hook)
+        except:
+            pass
