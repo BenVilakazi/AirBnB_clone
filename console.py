393b3b2b0faa88592d2a4976fedddb3c5bc097ff
Ben Vilakazi <xolanithengubane@gmail.com>
2024-01-05 02:28:27 +0200

    ytr

--- console.py
+++ console.py
@@ -60,9 +60,66 @@ class HBNBCommand(cmd.Cmd):
         show:
             class name and id - print the string representation of an instance
             
-            
+            The method splits the arguments into smaller strings, 
+            handles IndexError and KeyError, 
+            and prints the result of class name and id to the screen.
         """
-    
+        if len(line) == 0:
+            print("** class name missing **")
+        else:
+            pattern = "[^\s\"\']+|\"[^\"]*\"|\'[^\']*\'"
+            pattern = re.compile(pattern)
+            line = re.findall(pattern, line)
+            for i in range(len(line)):
+                line[i] = line[i].strip("\"'")
+            if line[0] in models.class_dict:
+                try:
+                    obj_id = line[0] + '.' + line[1]
+                except IndexError:
+                    print("** instance id missing **")
+                else:
+                    try:
+                        obj = models.storage.all()[obj_id]
+                    except KeyError:
+                        print("** no instance found **")
+                    else:
+                        try:
+                            attr = line[2]
+                        except IndexError:
+                            print("** attribute name missing **")
+                        else:
+                            try:
+                                val = line[3]
+                            except IndexError:
+                                print("** value missing **")
+                            else:
+                                try:
+                                    setattr(obj, attr, val)
+                                except AttributeError:
+                                    print("** cannot set val: {}".format(val) +
+                                          " for attr: ({}) **".format(attr))
+                                else:
+                                    obj.save()
+            else:
+                print("** class doesn't exist **")
+                
+    def do_count(self,line):
+        """
+        Usage: count [<class>]
+        
+        Count the num of instance of class if provided, else
+        total num of instances in memory
+        """
+        if len(line) == 0:
+            print(len([str(x) for x in models.storage.all().values()]))
+        elif line not in models.class_dict:
+            print("** class doesn't exist **")
+        else:
+            print(len([str(x) for x, y in models.storage.all().items()
+                       if line in y]))
+            
+               
+                    
     
     
 if __name__ == '__main__':
